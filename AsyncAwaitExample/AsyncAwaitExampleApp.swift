8465e6c84fa0d7291c38e9c225bab5ec63c6c05f
ApiOSDev <attup23@gmail.com>
2024-09-28 15:39:02 -0400

    Initial Commit

---
+++ AsyncAwaitExample/AsyncAwaitExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  AsyncAwaitExampleApp.swift
+//  AsyncAwaitExample
+//
+//  Created by Ashesh Patel on 2024-09-28.
+//
+
+import SwiftUI
+
+@main
+struct AsyncAwaitExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AsyncAwaitExampleTests/AsyncAwaitExampleTests.swift
@@ -0,0 +1,17 @@
+//
+//  AsyncAwaitExampleTests.swift
+//  AsyncAwaitExampleTests
+//
+//  Created by Ashesh Patel on 2024-09-28.
+//
+
+import Testing
+@testable import AsyncAwaitExample
+
+struct AsyncAwaitExampleTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
